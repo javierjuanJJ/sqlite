14a74f82720524b90dc8bc477b40e4d24f0de509
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-07 10:42:35 +0100

    Create A Table

--- database.py
+++ database.py
@@ -1,3 +1,19 @@
 import sqlite3
 # connection = sqlite3.connect(':memory:')
 connection = sqlite3.connect('customer.db')
+
+cursor = connection.cursor()
+
+cursor.execute('''
+    CREATE TABLE customers(
+        first_name text,
+        last_name text,
+        email text,
+    )
+''')
+
+
+connection.commit()
+
+
+connection.close()
